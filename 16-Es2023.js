5e1ce9a42c7230ed65166e4e1b79e621fc5678c3
worksofallwin <princeallwin199@gmail.com>
2023-10-14 16:39:54 +0530

    add es2023

---
+++ 16-Es2023.js
@@ -0,0 +1,6 @@
+// array.findLast
+// array.findLastIndex
+// Array.prototype.toReversed() -> Array
+// Array.prototype.toSorted(compareFn) -> Array
+// Array.prototype.toSpliced(start, deleteCount, ...items) -> Array
+// Array.prototype.with(index, value) -> Array
